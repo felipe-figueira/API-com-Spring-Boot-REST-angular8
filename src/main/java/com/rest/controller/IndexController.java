b8d393e825f0ee7453acd0dd6764526b466d3629
felipe-figueira <felipe.figueira13@gmail.com>
2020-04-13 02:11:30 -0300

    add pesquisa de usuário

--- src/main/java/com/rest/controller/IndexController.java
+++ src/main/java/com/rest/controller/IndexController.java
@@ -1,15 +1,38 @@
 package com.rest.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rest.model.Usuario;
+import com.rest.repository.UsuarioRepository;
 
 @RestController
 public class IndexController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
-	@GetMapping(value = "/" , produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá Spring boot", HttpStatus.OK);
+	@GetMapping(value = "/{id}" , produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+	
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuario(){
+		
+		 List<Usuario> list = usuarioRepository.findAll();
+		 
+		 return new ResponseEntity<List<Usuario>>(list,  HttpStatus.OK);
 	}
 }
