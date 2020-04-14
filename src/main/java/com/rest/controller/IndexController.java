9182a89e3b3ddf3274479f2c38bc1d0bce7335f8
felipe-figueira <felipe.figueira13@gmail.com>
2020-04-14 03:19:24 -0300

    add put/delete

--- src/main/java/com/rest/controller/IndexController.java
+++ src/main/java/com/rest/controller/IndexController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,4 +47,21 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 		}
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> update(@RequestBody Usuario usuario){
+		Usuario updateUsuer = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(updateUsuer, HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/json")
+	public String delete(@PathVariable (value = "id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+	
+		
+		return "ok";
+	}
 }
